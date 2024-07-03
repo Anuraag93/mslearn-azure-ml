c2f9b70039660f773e1a0986aa964675f3caacb9
Anurag Banger <abanger@discoveryparks.com.au>
2024-07-04 03:39:30 +1000

    2 instances is needed for the lab

--- Labs/11/setup.sh
+++ Labs/11/setup.sh
@@ -34,4 +34,4 @@ az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type C
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 1 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
